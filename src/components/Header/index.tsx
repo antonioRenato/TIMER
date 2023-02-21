6a313ee897e04292ae46a1a09db2fdd3c66f9b26
antonioRenato <antoniorpfs@gmail.com>
2023-02-21 04:15:18 -0300

    add logo and installed phospho-react

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>logo</span>
+      <img src={logoIgnite} alt="" />
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
       </nav>
     </HeaderContainer>
   )
